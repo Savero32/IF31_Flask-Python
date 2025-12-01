8d5b4d6c3d37025e67676cb5d4d2acc31529a135
Savero32 <savero.sadha@gmail.com>
2025-12-01 20:34:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify, request
+from flask import Flask, jsonify, request
 import pandas as pd
 import joblib
 from flask_cors import CORS
@@ -7,48 +7,43 @@ app = Flask(__name__)
 CORS(app)
 
 loaded_model = joblib.load("models/model_diabetes.pkl")
-
 loaded_scaler = joblib.load("models/standard_scaler.pkl")
 
 columns = [
-        'Pregnancies', 
-        'Glucose', 
-        'BloodPressure', 
-        'SkinThickness', 
-        'Insulin', 
-        'BMI', 
-        'DiabetesPedigreeFunction', 
-        'Age'
-    ]
+    'Pregnancies', 
+    'Glucose', 
+    'BloodPressure', 
+    'SkinThickness', 
+    'Insulin', 
+    'BMI', 
+    'DiabetesPedigreeFunction', 
+    'Age'
+]
 
 @app.route('/')
 def index():
     return jsonify({
-        "meta" : {
-            "status" : "success",
-            "message" : "Welcome to Diabetes"
+        "meta": {
+            "status": "success",
+            "message": "Welcome to Diabetes"
         },
-        "data" : None
+        "data": None
     })
 
-@app.route('/api/predict', methods =["POST"])
+@app.route('/api/predict', methods=["POST"])
 def predict():
     data = request.get_json()
-
     X_input = pd.DataFrame([data], columns=columns)
-
     X_input_scaled = loaded_scaler.transform(X_input)
-
-    
     prediction = loaded_model.predict(X_input_scaled)
-
+    
     return jsonify({
         "meta": {
-            "status" : "Success",
-            "message" : "Prediction"
+            "status": "success",
+            "message": "Prediction"
         },
-        "data" : prediction.tolist()[0]
+        "data": int(prediction[0])
     })
 
 if __name__ == '__main__': 
-    app.run(debug = True)
+    app.run(debug=True)
